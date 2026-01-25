7a353fea0ec03692a8b107b81214ae86a2d0770e
SoWiEee <kawiisowieee@gmail.com>
2026-01-25 14:59:33 +0800

    Fix kernel variable type

--- driver/include/miniedr_ioctl.h
+++ driver/include/miniedr_ioctl.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <stdint.h>
 
 // Device name: \\.\MiniEDRDrv  (symbolic link to \Device\MiniEDRDrv)
 #define MINIEDR_DEVICE_DOS_NAME        L"\\\\.\\\\MiniEDRDrv"
@@ -7,12 +6,9 @@
 #define MINIEDR_DEVICE_SYMBOLIC_LINK   L"\\DosDevices\\MiniEDRDrv"
 
 #define MINIEDR_IOCTL_VERSION 0x00010000u
-#ifndef FILE_DEVICE_UNKNOWN
-#define FILE_DEVICE_UNKNOWN 0x00000022
-#endif
 
 // Event types
-typedef enum _MINIEDR_EVENT_TYPE : uint32_t {
+typedef enum _MINIEDR_EVENT_TYPE {
     MiniEdrEvent_Invalid = 0,
     MiniEdrEvent_ProcessCreate = 1,
     MiniEdrEvent_ProcessExit   = 2,
@@ -25,31 +21,31 @@ typedef enum _MINIEDR_EVENT_TYPE : uint32_t {
 #pragma pack(push, 1)
 
 typedef struct _MINIEDR_EVENT_HEADER {
-    uint32_t Type;       // MINIEDR_EVENT_TYPE
-    uint32_t Size;       // total size of event including header
-    uint64_t TimestampQpc; // KeQueryPerformanceCounter() value
+    UINT32 Type;       // MINIEDR_EVENT_TYPE
+    UINT32 Size;       // total size of event including header
+    UINT64 TimestampQpc; // KeQueryPerformanceCounter() value
 } MINIEDR_EVENT_HEADER;
 
 typedef struct _MINIEDR_EVT_PROCESS {
     MINIEDR_EVENT_HEADER H;
-    uint32_t Pid;
-    uint32_t ParentPid;
-    uint8_t  ImageFileName[16]; // EPROCESS ImageFileName (ANSI, truncated)
+    UINT32 Pid;
+    UINT32 ParentPid;
+    UINT8 ImageFileName[16]; // EPROCESS ImageFileName (ANSI, truncated)
 } MINIEDR_EVT_PROCESS;
 
 typedef struct _MINIEDR_EVT_IMAGELOAD {
     MINIEDR_EVENT_HEADER H;
-    uint32_t Pid;
-    uint32_t Reserved;
+    UINT32 Pid;
+    UINT32 Reserved;
     wchar_t  ImagePath[260]; // FullImageName (truncated)
 } MINIEDR_EVT_IMAGELOAD;
 
 typedef struct _MINIEDR_EVT_HANDLEACCESS {
     MINIEDR_EVENT_HEADER H;
-    uint32_t SourcePid;
-    uint32_t TargetPid;
-    uint32_t DesiredAccess;
-    uint32_t Operation; // 1=CreateHandle,2=DuplicateHandle (best-effort)
+    UINT32 SourcePid;
+    UINT32 TargetPid;
+    UINT32 DesiredAccess;
+    UINT32 Operation; // 1=CreateHandle,2=DuplicateHandle (best-effort)
 } MINIEDR_EVT_HANDLEACCESS;
 
 #pragma pack(pop)
@@ -60,15 +56,31 @@ typedef struct _MINIEDR_EVT_HANDLEACCESS {
 #define IOCTL_MINIEDR_GET_VERSION CTL_CODE(FILE_DEVICE_UNKNOWN, MINIEDR_IOCTL_BASE + 0, METHOD_BUFFERED, FILE_READ_ACCESS)
 #define IOCTL_MINIEDR_GET_EVENTS  CTL_CODE(FILE_DEVICE_UNKNOWN, MINIEDR_IOCTL_BASE + 1, METHOD_BUFFERED, FILE_READ_ACCESS)
 #define IOCTL_MINIEDR_SET_POLICY  CTL_CODE(FILE_DEVICE_UNKNOWN, MINIEDR_IOCTL_BASE + 2, METHOD_BUFFERED, FILE_WRITE_ACCESS)
+#define IOCTL_MINIEDR_SET_POLICY_V2 CTL_CODE(FILE_DEVICE_UNKNOWN, MINIEDR_IOCTL_BASE + 3, METHOD_BUFFERED, FILE_WRITE_ACCESS)
 
 // GET_VERSION output
 typedef struct _MINIEDR_VERSION_INFO {
-    uint32_t Version; // MINIEDR_IOCTL_VERSION
-    uint32_t Features; // bitmask (future use)
+    UINT32 Version; // MINIEDR_IOCTL_VERSION
+    UINT32 Features; // bitmask (future use)
 } MINIEDR_VERSION_INFO;
 
 // SET_POLICY input (minimal; expandable)
 typedef struct _MINIEDR_POLICY {
-    uint32_t EnableHandleAudit; // 0/1
-    uint32_t Reserved;
+    UINT32 EnableHandleAudit; // 0/1
+    UINT32 Reserved;
 } MINIEDR_POLICY;
+
+// Policy v2 (variable length PID lists)
+#define MINIEDR_POLICY_FLAG_ENFORCE_PROTECT 0x00000001u
+
+#pragma pack(push, 1)
+typedef struct _MINIEDR_POLICY_V2 {
+    UINT32 Version;         // set to MINIEDR_IOCTL_VERSION
+    UINT32 Flags;           // MINIEDR_POLICY_FLAG_*
+    UINT32 ProtectedPidCount;
+    UINT32 AllowedPidCount;
+    // Followed by arrays:
+    // uint32_t ProtectedPids[ProtectedPidCount];
+    // uint32_t AllowedPids[AllowedPidCount];
+} MINIEDR_POLICY_V2;
+#pragma pack(pop)
